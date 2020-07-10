3d774f26e99ff5a94745e81020411ac67bf7668c
YOSHIKI SAWA <y.sawa@kizinosu.com>
2020-07-11 01:15:38 +0900

    [lesson2] CSSを適応する

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,5 @@
-function Layout({ childeren }) {
-    return <di>{childeren}</di>
-}
+import styles from './layout.module.css'
 
-export default Layout
+export default function Layout({ children }) {
+  return <div className={styles.container}>{children}</div>
+}
