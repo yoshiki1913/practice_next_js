1d8bffbdc8b782cd0da82c7cece7735f36fdb65d
YOSHIKI SAWA <y.sawa@kizinosu.com>
2020-07-11 00:23:52 +0900

    [lesson2] メタデータの設定

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,12 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default function FirstPost() {
     return (
         <>
+        <Head>
+            <title>FIrst Post</title>
+        </Head>
         <h1>First Post</h1>
         <h2>
             <Link href="/">
