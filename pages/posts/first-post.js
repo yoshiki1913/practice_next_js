d00edfba8b5135ee4b00ae537f6735552973cfa4
YOSHIKI SAWA <y.sawa@kizinosu.com>
2020-07-11 00:05:45 +0900

    [lesson1] <Link>を使う

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,14 @@
+import Link from 'next/link'
+
 export default function FirstPost() {
-    return <h1>First Post</h1>
+    return (
+        <>
+        <h1>First Post</h1>
+        <h2>
+            <Link href="/">
+                <a>Back to Home</a>
+            </Link>
+        </h2>
+        </>
+    )
 }
